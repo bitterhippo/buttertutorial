ae496da6fd0d7bbf38682988cc896b7700eb9173
Noah <bitterhippo@gmail.com>
2021-07-25 10:07:07 +0900

    basic navbar configuration and some styling

--- src/components/navigation/NavHeader.js
+++ src/components/navigation/NavHeader.js
@@ -3,20 +3,33 @@ import React from 'react';
 const NavHeader = () => {
   return (
     <div style={styles.navWrapper}>
-      <div>
-        Xplenty
+      <div style={styles.navBarContent}>
+        <div>
+          Xplenty
+        </div>
+        <div>
+          Product
+        </div>
+        <div>
+          Integrations
+        </div>
+        <div>
+          Solutions
+        </div>
+        <div>
+          Resources
+        </div>
       </div>
-      <div>
-        Product
-      </div>
-      <div>
-        Integrations
-      </div>
-      <div>
-        Solutions
-      </div>
-      <div>
-        Resources
+      <div style={styles.navBarConfiguration}>
+        <div>
+          Language
+        </div>
+        <div>
+          Sign In
+        </div>
+        <div>
+          GET STARTED
+        </div>
       </div>
     </div>
   )
@@ -25,7 +38,28 @@ const NavHeader = () => {
 const styles = {
   navWrapper: {
     display: 'flex',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    border: '2px solid gray'
+  },
+  navBarContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: '3vw',
+    marginTop: '1vh',
+    width: '40%',
+    gap: '2vw',
+    height: '3vh',
+  },
+  navBarConfiguration: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginLeft: '3vw',
+    marginTop: '1vh',
+    gap: '2vw',
+    height: '3vh',
+    marginRight: '3vh'
   }
 }
 
